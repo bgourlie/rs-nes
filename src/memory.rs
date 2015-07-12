edc8a714a4b5bb452f6deb2b00f5c397e039d231
W. Brian Gourlie <bgourlie@gmail.com>
2015-07-12 01:21:29 -0500

    Use const instead of hardcoded value

--- src/memory.rs
+++ src/memory.rs
@@ -2,15 +2,17 @@ pub struct Memory {
   addr:[u8; 0xffff]
 }
 
+const ADDRESSABLE_MEMORY: usize = 0xFFFF;
+
 impl Memory {
   pub fn new() -> Memory {
     Memory {
-      addr: [0; 0xffff]
+      addr: [0; ADDRESSABLE_MEMORY]
     }
   }
 
   pub fn store(&mut self, addr: usize, data: u8) -> Result<(), &'static str> {
-    if addr > 0xffff {
+    if addr > ADDRESSABLE_MEMORY {
       Err("memory address out of bounds")
     } else {
       self.addr[addr] = data;
@@ -19,7 +21,7 @@ impl Memory {
   }
 
   pub fn load(&mut self, addr: usize) -> Result<u8, &'static str> {
-    if addr > 0xffff {
+    if addr > ADDRESSABLE_MEMORY {
       Err("memory address out of bounds")
     } else {
       Ok(self.addr[addr])
