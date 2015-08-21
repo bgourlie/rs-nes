ccc5ddaae9521cc7b39e7f1bbabf35eb933f57a4
W. Brian Gourlie <bgourlie@gmail.com>
2015-08-21 08:53:16 -0500

    Remove nes specific memory mapping

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,25 +1,3 @@
-// NES MEMORY MAP
-// http://emudocs.org/NES/nestech.txt
-//
-// +---------+-------+-------+-----------------------+
-// | Address | Size  | Flags | Description           |
-// +---------+-------+-------+-----------------------+
-// | $0000   | $800  |       | RAM                   |
-// | $0800   | $800  | M     | RAM                   |
-// | $1000   | $800  | M     | RAM                   |
-// | $1800   | $800  | M     | RAM                   |
-// | $2000   | 8     |       | Registers             |
-// | $2008   | $1FF8 |  R    | Registers             |
-// | $4000   | $20   |       | Registers             |
-// | $4020   | $1FDF |       | Expansion ROM         |
-// | $6000   | $2000 |       | SRAM                  |
-// | $8000   | $4000 |       | PRG-ROM               |
-// | $C000   | $4000 |       | PRG-ROM               |
-// +---------+-------+-------+-----------------------+
-//        Flag Legend: M = Mirror of $0000
-//                     R = Mirror of $2000-2008 every 8 bytes
-//                         (e.g. $2008=$2000, $2018=$2000, etc.)
-
 #[cfg(test)]
 mod spec_tests;
 
@@ -38,26 +16,7 @@ impl Memory {
 
   pub fn store(&mut self, addr: u16, data: u8) {
     let addr = addr as usize;
-    match addr {
-      0x0 ... 0x7FF => {
-        // mirrored ram
-        self.addr[addr] = data;
-        self.addr[addr + 0x800] = data;
-        self.addr[addr + 0x1000] = data;
-        self.addr[addr + 0x1800] = data;
-      },
-      0x800 ... 0x1FFF => panic!("write to mirrored memory"),
-      0x2000 ... 0x401F => {
-        // registers
-        self.addr[addr] = data;
-      },
-      0x4020 ... 0x5FFF | 0x8000 ... 0xEFFF => panic!("write to rom"),
-      0x6000 ... 0x7FFF => {
-        // sram
-        self.addr[addr] = data;
-      },
-      _ => panic!("memory access out of bounds")
-    }
+    self.addr[addr] = data;
   }
 
   pub fn store16(&mut self, addr: u16, data: u16) {
--- src/memory/spec_tests.rs
+++ src/memory/spec_tests.rs
@@ -19,19 +19,6 @@ fn store16_and_load16() {
 }
 
 #[test]
-fn ram_should_be_mirrored() {
-  let mut mem = Memory::new();
-  let stored = 0xB;
-  let _ = mem.store(0x0, stored);
-  let mirror1 = mem.load(0x800);
-  let mirror2 = mem.load(0x1000);
-  let mirror3 = mem.load(0x1800);
-  assert_eq!(stored, mirror1);
-  assert_eq!(stored, mirror2);
-  assert_eq!(stored, mirror3);
-}
-
-#[test]
 fn load16_zp_indexed_should_wrap_on_carry() {
   let mut mem = Memory::new();
   mem.store16(0x20, 0xbeef);
@@ -47,18 +34,4 @@ fn load16_zp_indexed_shouldnt_wrap_no_carry() {
   mem.store16(0x20, 0xbeef);
   let val = mem.load16_zp_indexed(0x10, 0x10);
   assert_eq!(0xbeef, val);
-}
-
-#[test]
-#[should_panic(expected = "write to mirrored memory")]
-fn write_to_mirrored_ram_should_error() {
-  let mut mem = Memory::new();
-  mem.store(0x1000, 0x0);
-}
-
-#[test]
-#[should_panic(expected = "write to rom")]
-fn store_to_rom_should_error() {
-  let mut mem = Memory::new();
-  mem.store(0x8000, 0x0);
 }
