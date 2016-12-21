7a073f87cbb648a43d8408633b896eca7404f6e6
W. Brian Gourlie <bgourlie@gmail.com>
2016-12-20 23:26:22 -0600

    Implement status register

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,45 +1,15 @@
 mod control_register;
 mod mask_register;
+mod status_register;
 
 use self::control_register::ControlRegister;
+use self::mask_register::MaskRegister;
+use self::status_register::StatusRegister;
 
 pub struct Ppu {
     ctrl_reg: ControlRegister,
+    mask_reg: MaskRegister,
+    status_reg: StatusRegister
 }
 
 impl Ppu {}
-
-
-/// $2002, Read Only
-struct PpuStatus {
-    reg: u8,
-}
-
-/// $2003, Write Only
-struct OamAddr {
-    reg: u8,
-}
-
-/// $2004, Read/Write
-struct OamData {
-}
-
-/// $2005, Write (2X)
-struct PpuScroll {
-
-}
-
-/// $2006, Write (2X)
-struct PpuAddr {
-
-}
-
-/// $2007, Read/Write
-struct PpuData {
-
-}
-
-/// $4014, Write
-struct OamDma {
-
-}
