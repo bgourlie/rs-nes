b937d09a8697ecec9612284e4e16405dc8abf581
W. Brian Gourlie <bgourlie@gmail.com>
2016-12-21 19:37:31 -0600

    Add all the trivial registers

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -7,9 +7,15 @@ use self::mask_register::MaskRegister;
 use self::status_register::StatusRegister;
 
 pub struct Ppu {
-    ctrl_reg: ControlRegister,
-    mask_reg: MaskRegister,
-    status_reg: StatusRegister
+    ctrl_reg: ControlRegister, // 0x2000
+    mask_reg: MaskRegister, // 0x2001
+    status_reg: StatusRegister, // 0x2002
+    oam_addr: u8, // 0x2003
+    oam_data: u8, // 0x2004
+    scroll: u8, // 0x2005
+    vram_addr: u8, // 0x2006
+    vram_data: u8, // 0x2007
+    oam_dma: u8, // 0x4014
 }
 
 impl Ppu {}
