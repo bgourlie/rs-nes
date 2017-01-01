0f5fa5711177d302baa25c9f85a74d01e0e63844
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-01 16:17:09 -0600

    Implement bmi

--- src/cpu/opcodes/bmi/mod.rs
+++ src/cpu/opcodes/bmi/mod.rs
@@ -1,5 +1,19 @@
+#[cfg(test)]
+mod spec_tests;
+
+use cpu::Cpu;
+use memory::Memory;
+use super::addressing_mode::AddressingMode;
+use super::branch_utils::branch;
 use super::OpCode;
 
 pub struct Bmi;
 
-impl OpCode for Bmi {}
+impl OpCode for Bmi {
+    fn execute<M: Memory, AM: AddressingMode<M>, F: Fn(&Cpu<M>)>(cpu: &mut Cpu<M>,
+                                                                 am: AM,
+                                                                 tick_handler: &F) {
+        let sign_set = cpu.registers.sign_flag();
+        branch(cpu, am, tick_handler, sign_set);
+    }
+}
