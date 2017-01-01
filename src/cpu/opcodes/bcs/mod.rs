3a3ab1bd8c120daaef1cf9485911bedbf512abaf
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-01 14:52:00 -0600

    Implement BCS

--- src/cpu/opcodes/bcs/mod.rs
+++ src/cpu/opcodes/bcs/mod.rs
@@ -1,5 +1,19 @@
+#[cfg(test)]
+mod spec_tests;
+
+use cpu::Cpu;
+use memory::Memory;
+use super::addressing_mode::AddressingMode;
+use super::branch_utils::branch;
 use super::OpCode;
 
 pub struct Bcs;
 
-impl OpCode for Bcs {}
+impl OpCode for Bcs {
+    fn execute<M: Memory, AM: AddressingMode<M>, F: Fn(&Cpu<M>)>(cpu: &mut Cpu<M>,
+                                                                 am: AM,
+                                                                 tick_handler: &F) {
+        let carry_set = cpu.registers.carry_flag();
+        branch(cpu, am, tick_handler, carry_set);
+    }
+}
---
+++ src/cpu/opcodes/bcs/spec_tests.rs
@@ -0,0 +1,43 @@
+use cpu::opcodes::OpCode;
+use cpu::opcodes::branch_utils::spec_tests::*;
+use super::Bcs;
+
+#[test]
+fn test_branch_not_crossing_page_boundary_positive_rel_addr() {
+    branch_not_crossing_page_boundary_positive_rel_addr(|ref mut cpu, rel_addr| {
+        cpu.registers.set_carry_flag(true);
+        Bcs::execute_test(cpu, rel_addr)
+    });
+}
+
+#[test]
+fn test_branch_not_crossing_page_boundary_negative_rel_addr() {
+    branch_not_crossing_page_boundary_negative_rel_addr(|ref mut cpu, rel_addr| {
+        cpu.registers.set_carry_flag(true);
+        Bcs::execute_test(cpu, rel_addr)
+    });
+}
+
+#[test]
+fn test_branch_crossing_page_boundary_positive_rel_addr() {
+    branch_crossing_page_boundary_positive_rel_addr(|ref mut cpu, rel_addr| {
+        cpu.registers.set_carry_flag(true);
+        Bcs::execute_test(cpu, rel_addr)
+    });
+}
+
+#[test]
+fn test_branch_crossing_page_boundary_negative_rel_addr() {
+    branch_crossing_page_boundary_negative_rel_addr(|ref mut cpu, rel_addr| {
+        cpu.registers.set_carry_flag(true);
+        Bcs::execute_test(cpu, rel_addr)
+    });
+}
+
+#[test]
+fn test_no_branch() {
+    no_branch(|ref mut cpu, rel_addr| {
+        cpu.registers.set_carry_flag(false);
+        Bcs::execute_test(cpu, rel_addr)
+    });
+}
