a1decc02c6832e6de52e904e2b9a58c79930a0e6
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-01 17:13:10 -0600

    Fix build

--- src/cpu/opcodes/brk/mod.rs
+++ src/cpu/opcodes/brk/mod.rs
@@ -16,19 +16,19 @@ impl OpCode for Brk {
                                                                  _: AM,
                                                                  tick_handler: &F) {
         cpu.registers.pc += 1;
-        tick_handler();
+        tick_handler(cpu);
         let (pc_low_byte, pc_high_byte) = lo_hi(cpu.registers.pc);
         let status = cpu.registers.status;
         cpu.push_stack(pc_low_byte);
-        tick_handler();
+        tick_handler(cpu);
         cpu.push_stack(pc_high_byte);
-        tick_handler();
+        tick_handler(cpu);
         cpu.push_stack(status);
-        tick_handler();
+        tick_handler(cpu);
         let irq_handler_low = cpu.memory.load(BRK_VECTOR);
-        tick_handler();
+        tick_handler(cpu);
         let irq_handler_high = cpu.memory.load(BRK_VECTOR + 1);
-        tick_handler();
+        tick_handler(cpu);
         cpu.registers.pc = from_lo_hi(irq_handler_low, irq_handler_high);
         cpu.registers.set_interrupt_disable_flag(true);
     }
