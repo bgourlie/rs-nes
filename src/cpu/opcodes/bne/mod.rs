524a479832c0d5da1e1e41587a5590a0880c0647
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-01 16:21:01 -0600

    Implement bne

--- src/cpu/opcodes/bne/mod.rs
+++ src/cpu/opcodes/bne/mod.rs
@@ -1,5 +1,19 @@
+#[cfg(test)]
+mod spec_tests;
+
+use cpu::Cpu;
+use memory::Memory;
+use super::addressing_mode::AddressingMode;
+use super::branch_utils::branch;
 use super::OpCode;
 
 pub struct Bne;
 
-impl OpCode for Bne {}
+impl OpCode for Bne {
+    fn execute<M: Memory, AM: AddressingMode<M>, F: Fn(&Cpu<M>)>(cpu: &mut Cpu<M>,
+                                                                 am: AM,
+                                                                 tick_handler: &F) {
+        let zero_clear = !cpu.registers.zero_flag();
+        branch(cpu, am, tick_handler, zero_clear);
+    }
+}
