809e0aba818b5bb8fc529fa3b62aecfcebebc5dc
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-01 16:27:37 -0600

    Implement bpl

---
+++ src/cpu/opcodes/bpl/spec_tests.rs
@@ -0,0 +1,43 @@
+use cpu::opcodes::OpCode;
+use cpu::opcodes::branch_utils::spec_tests::*;
+use super::Bpl;
+
+#[test]
+fn test_branch_not_crossing_page_boundary_positive_offset() {
+    branch_not_crossing_page_boundary_positive_offset(|ref mut cpu, offset| {
+        cpu.registers.set_sign_flag(false);
+        Bpl::execute_cycles(cpu, offset)
+    });
+}
+
+#[test]
+fn test_branch_not_crossing_page_boundary_negative_offset() {
+    branch_not_crossing_page_boundary_negative_offset(|ref mut cpu, offset| {
+        cpu.registers.set_sign_flag(false);
+        Bpl::execute_cycles(cpu, offset)
+    });
+}
+
+#[test]
+fn test_branch_crossing_page_boundary_positive_offset() {
+    branch_crossing_page_boundary_positive_offset(|ref mut cpu, offset| {
+        cpu.registers.set_sign_flag(false);
+        Bpl::execute_cycles(cpu, offset)
+    });
+}
+
+#[test]
+fn test_branch_crossing_page_boundary_negative_rel_addr() {
+    branch_crossing_page_boundary_negative_offset(|ref mut cpu, offset| {
+        cpu.registers.set_sign_flag(false);
+        Bpl::execute_cycles(cpu, offset)
+    });
+}
+
+#[test]
+fn test_no_branch() {
+    no_branch(|ref mut cpu, offset| {
+        cpu.registers.set_sign_flag(true);
+        Bpl::execute_cycles(cpu, offset)
+    });
+}
