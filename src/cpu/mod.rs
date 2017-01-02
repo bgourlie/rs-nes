14a7a78e8b6695cefcddb22b1f2a76aba38245e1
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-01 21:46:52 -0600

    Wrapping convenience methods

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -2,10 +2,9 @@ mod byte_utils;
 mod registers;
 mod opcodes;
 
-use std::num::Wrapping;
 use memory::*;
 pub use self::registers::Registers;
-use self::byte_utils::{lo_hi, from_lo_hi};
+use self::byte_utils::{lo_hi, from_lo_hi, wrapping_inc, wrapping_dec};
 
 pub const STACK_LOC: u16 = 0x100;
 pub const NMI_VECTOR: u16 = 0xfffa;
@@ -42,20 +41,20 @@ impl<Mem: Memory> Cpu<Mem> {
     }
 
     pub fn reset(&mut self) {
-        let pc_start_low = self.memory.load(RESET_VECTOR);
-        let pc_start_high = self.memory.load(RESET_VECTOR + 1);
-        self.registers.pc = from_lo_hi(pc_start_low, pc_start_high);
+        let pc_low = self.memory.load(RESET_VECTOR);
+        let pc_high = self.memory.load(RESET_VECTOR + 1);
+        self.registers.pc = from_lo_hi(pc_low, pc_high);
     }
 
     pub fn nmi(&mut self) {
         let stat = self.registers.status;
-        let (pc_low_byte, pc_high_byte) = lo_hi(self.registers.pc);
-        self.push_stack(pc_low_byte);
-        self.push_stack(pc_high_byte);
+        let (pc_low, pc_high) = lo_hi(self.registers.pc);
+        self.push_stack(pc_low);
+        self.push_stack(pc_high);
         self.push_stack(stat);
-        let pc_low_byte = self.memory.load(NMI_VECTOR);
-        let pc_high_byte = self.memory.load(NMI_VECTOR + 1);
-        self.registers.pc = from_lo_hi(pc_low_byte, pc_high_byte);
+        let pc_low = self.memory.load(NMI_VECTOR);
+        let pc_high = self.memory.load(NMI_VECTOR + 1);
+        self.registers.pc = from_lo_hi(pc_low, pc_high);
     }
 
     fn read_op(&mut self) -> u8 {
@@ -67,13 +66,13 @@ impl<Mem: Memory> Cpu<Mem> {
 
     fn push_stack(&mut self, value: u8) {
         self.memory.store(STACK_LOC + self.registers.sp as u16, value);
-        self.registers.sp = (Wrapping(self.registers.sp) - Wrapping(1)).0;
+        self.registers.sp = wrapping_dec(self.registers.sp);
     }
 
     fn pop_stack(&mut self) -> u8 {
         let val = self.memory
-            .load(STACK_LOC + ((Wrapping(self.registers.sp) + Wrapping(1)).0) as u16);
-        self.registers.sp = (Wrapping(self.registers.sp) + Wrapping(1)).0;
+            .load(STACK_LOC + wrapping_inc(self.registers.sp) as u16);
+        self.registers.sp = wrapping_inc(self.registers.sp);
         val
     }
 }
