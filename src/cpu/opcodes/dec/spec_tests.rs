9d2c26484a82707a853059d4280ca8270c5cc361
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-02 14:00:00 -0600

    Made inc/dec tests more generic

--- src/cpu/opcodes/dec/spec_tests.rs
+++ src/cpu/opcodes/dec/spec_tests.rs
@@ -1,24 +1,34 @@
+use cpu::opcodes::addressing_mode::test_addressing_modes::WriterAddressingMode;
 use cpu::opcodes::inc_dec_tests_base::*;
 use cpu::opcodes::OpCode;
 use super::Dec;
 
 #[test]
 fn test1() {
-    dec_base_1(|ref mut cpu, am| {
+    dec_base_1(|ref mut cpu, val| {
+        let am = WriterAddressingMode::new(val);
+        let value_ref = am.value_ref();
         Dec::execute_cycles(cpu, am);
+        value_ref.get()
     });
 }
 
 #[test]
 fn test2() {
-    dec_base_2(|ref mut cpu, am| {
+    dec_base_2(|ref mut cpu, val| {
+        let am = WriterAddressingMode::new(val);
+        let value_ref = am.value_ref();
         Dec::execute_cycles(cpu, am);
+        value_ref.get()
     });
 }
 
 #[test]
 fn test3() {
-    dec_base_3(|ref mut cpu, am| {
+    dec_base_3(|ref mut cpu, val| {
+        let am = WriterAddressingMode::new(val);
+        let value_ref = am.value_ref();
         Dec::execute_cycles(cpu, am);
+        value_ref.get()
     });
 }
--- src/cpu/opcodes/inc/spec_tests.rs
+++ src/cpu/opcodes/inc/spec_tests.rs
@@ -1,24 +1,34 @@
+use cpu::opcodes::addressing_mode::test_addressing_modes::WriterAddressingMode;
 use cpu::opcodes::inc_dec_tests_base::*;
 use cpu::opcodes::OpCode;
 use super::Inc;
 
 #[test]
 fn test1() {
-    inc_base_1(|ref mut cpu, am| {
+    inc_base_1(|ref mut cpu, val| {
+        let am = WriterAddressingMode::new(val);
+        let value_ref = am.value_ref();
         Inc::execute_cycles(cpu, am);
+        value_ref.get()
     });
 }
 
 #[test]
 fn test2() {
-    inc_base_2(|ref mut cpu, am| {
+    inc_base_2(|ref mut cpu, val| {
+        let am = WriterAddressingMode::new(val);
+        let value_ref = am.value_ref();
         Inc::execute_cycles(cpu, am);
+        value_ref.get()
     });
 }
 
 #[test]
 fn test3() {
-    inc_base_3(|ref mut cpu, am| {
+    inc_base_3(|ref mut cpu, val| {
+        let am = WriterAddressingMode::new(val);
+        let value_ref = am.value_ref();
         Inc::execute_cycles(cpu, am);
+        value_ref.get()
     });
 }
