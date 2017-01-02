3ad94a925984c0471a3f2995604e8676c5730418
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-01 18:04:22 -0600

    Implement bvc, bvs

---
+++ src/cpu/opcodes/bvc/spec_tests.rs
@@ -0,0 +1,43 @@
+use cpu::opcodes::OpCode;
+use cpu::opcodes::branch_utils::spec_tests::*;
+use super::Bvc;
+
+#[test]
+fn test_branch_not_crossing_page_boundary_positive_offset() {
+    branch_not_crossing_page_boundary_positive_offset(|ref mut cpu, offset| {
+        cpu.registers.set_overflow_flag(false);
+        Bvc::execute_cycles(cpu, offset)
+    });
+}
+
+#[test]
+fn test_branch_not_crossing_page_boundary_negative_offset() {
+    branch_not_crossing_page_boundary_negative_offset(|ref mut cpu, offset| {
+        cpu.registers.set_overflow_flag(false);
+        Bvc::execute_cycles(cpu, offset)
+    });
+}
+
+#[test]
+fn test_branch_crossing_page_boundary_positive_offset() {
+    branch_crossing_page_boundary_positive_offset(|ref mut cpu, offset| {
+        cpu.registers.set_overflow_flag(false);
+        Bvc::execute_cycles(cpu, offset)
+    });
+}
+
+#[test]
+fn test_branch_crossing_page_boundary_negative_rel_addr() {
+    branch_crossing_page_boundary_negative_offset(|ref mut cpu, offset| {
+        cpu.registers.set_overflow_flag(false);
+        Bvc::execute_cycles(cpu, offset)
+    });
+}
+
+#[test]
+fn test_no_branch() {
+    no_branch(|ref mut cpu, offset| {
+        cpu.registers.set_overflow_flag(true);
+        Bvc::execute_cycles(cpu, offset)
+    });
+}
--- src/cpu/opcodes/bvs/mod.rs
+++ src/cpu/opcodes/bvs/mod.rs
@@ -1,5 +1,19 @@
+#[cfg(test)]
+mod spec_tests;
+
+use cpu::Cpu;
+use memory::Memory;
+use super::addressing_mode::AddressingMode;
+use super::branch_utils::branch;
 use super::OpCode;
 
 pub struct Bvs;
 
-impl OpCode for Bvs {}
+impl OpCode for Bvs {
+    fn execute<M: Memory, AM: AddressingMode<M>, F: Fn(&Cpu<M>)>(cpu: &mut Cpu<M>,
+                                                                 am: AM,
+                                                                 tick_handler: &F) {
+        let sign_clear = cpu.registers.overflow_flag();
+        branch(cpu, am, tick_handler, sign_clear);
+    }
+}
