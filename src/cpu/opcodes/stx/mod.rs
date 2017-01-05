284aa5470413e809a04273a334e12ac1e1c5ae3d
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-04 21:08:23 -0600

    Add todos

--- src/cpu/opcodes/stx/mod.rs
+++ src/cpu/opcodes/stx/mod.rs
@@ -9,6 +9,8 @@ use super::OpCode;
 pub struct Stx;
 
 impl OpCode for Stx {
+    // TODO: STX doesn't actually have an input
+    // Is there a compelling reason to have write-only addressing implementations?
     type Input = u8;
 
     fn execute<M, AM, F>(cpu: &mut Cpu<M>, am: AM, _: &F)
--- src/cpu/opcodes/sty/mod.rs
+++ src/cpu/opcodes/sty/mod.rs
@@ -9,6 +9,8 @@ use super::OpCode;
 pub struct Sty;
 
 impl OpCode for Sty {
+    // TODO: STY doesn't actually have an input
+    // Is there a compelling reason to have write-only addressing implementations?
     type Input = u8;
 
     fn execute<M, AM, F>(cpu: &mut Cpu<M>, am: AM, _: &F)
