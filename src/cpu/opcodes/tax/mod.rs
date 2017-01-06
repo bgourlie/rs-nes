7e23309c3df5feedeee292cb375df99abd2fcf40
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-05 22:16:29 -0600

    tax, tay

--- src/cpu/opcodes/tax/mod.rs
+++ src/cpu/opcodes/tax/mod.rs
@@ -1,7 +1,23 @@
+#[cfg(test)]
+mod spec_tests;
+
+use cpu::Cpu;
+use memory::Memory;
+use super::addressing::AddressingMode;
 use super::OpCode;
 
 pub struct Tax;
 
 impl OpCode for Tax {
     type Input = ();
+
+    fn execute<M, AM, F>(cpu: &mut Cpu<M>, _: AM, _: &F)
+        where M: Memory,
+              AM: AddressingMode<M, Output = Self::Input>,
+              F: Fn(&Cpu<M>)
+    {
+        cpu.registers.x = cpu.registers.acc;
+        let x = cpu.registers.x;
+        cpu.registers.set_sign_and_zero_flag(x);
+    }
 }
--- src/cpu/opcodes/tax/spec_tests.rs
+++ src/cpu/opcodes/tax/spec_tests.rs
@@ -1,1 +1,13 @@
-// TODO
+use cpu::*;
+use cpu::opcodes::addressing::Implied;
+use cpu::opcodes::OpCode;
+use super::Tax;
+
+#[test]
+fn tax() {
+    let mut cpu = TestCpu::new_test();
+    cpu.registers.acc = 0xff;
+    cpu.registers.x = 0x0;
+    Tax::execute_cycles(&mut cpu, Implied);
+    assert_eq!(0xff, cpu.registers.x);
+}
