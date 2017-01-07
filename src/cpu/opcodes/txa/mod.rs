d9bfdf494419ed23f6042a9d16d21a9134208b31
W. Brian Gourlie <bgourlie@gmail.com>
2017-01-07 12:52:40 -0600

    last few instructions implemented

--- src/cpu/opcodes/txa/mod.rs
+++ src/cpu/opcodes/txa/mod.rs
@@ -1,7 +1,23 @@
+#[cfg(test)]
+mod spec_tests;
+
+use cpu::Cpu;
+use memory::Memory;
+use super::addressing::AddressingMode;
 use super::OpCode;
 
 pub struct Txa;
 
 impl OpCode for Txa {
     type Input = ();
+
+    fn execute<M, AM, F>(cpu: &mut Cpu<M>, _: AM, _: &F)
+        where M: Memory,
+              AM: AddressingMode<M, Output = Self::Input>,
+              F: Fn(&Cpu<M>)
+    {
+        cpu.registers.acc = cpu.registers.x;
+        let acc = cpu.registers.acc;
+        cpu.registers.set_sign_and_zero_flag(acc);
+    }
 }
