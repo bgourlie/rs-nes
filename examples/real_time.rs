7255099762088867d0c08b85ec67a3b5cd1c49c3
W. Brian Gourlie <bgourlie@gmail.com>
2017-03-19 11:02:46 -0500

    cleanup

--- examples/real_time.rs
+++ examples/real_time.rs
@@ -1,13 +1,11 @@
 extern crate time;
 extern crate glium;
-extern crate glium_text;
 extern crate log;
 extern crate rs_nes;
 
 use glium::{Display, DisplayBuild, Surface, glutin};
 use glium::texture::RawImage2d;
 use glium::uniforms::MagnifySamplerFilter;
-use glium_text::{FontTexture, TextSystem};
 use rs_nes::cpu::*;
 use rs_nes::memory::nes_memory::NesMemoryImpl;
 use rs_nes::ppu::{Ppu, PpuImpl};
@@ -21,13 +19,11 @@ use std::rc::Rc;
 use std::thread;
 use std::time::{Duration, Instant};
 
-const NS_PER_SEC: f32 = 1000000000.0;
 static SCREEN_DIMENSIONS: (u32, u32) = (256, 240);
 
 fn main() {
     // INIT NES
-    let args: Vec<String> = env::args().collect();
-    let file = &args[args.len() - 1];
+    let file = env::args().last().unwrap();
 
     let rom = NesRom::read(format!("test_roms/{}", file)).unwrap();
     println!("ROM Mapper: {} CHR banks: {} CHR size: {}",
@@ -47,17 +43,7 @@ fn main() {
         .build_glium()
         .unwrap();
 
-    let system = TextSystem::new(&display);
-
-    // Font texture for drawing on buffer
-    let font = FontTexture::new(&display,
-                                File::open(&Path::new("examples/SourceCodePro-Regular.ttf"))
-                                    .unwrap(),
-                                60)
-        .unwrap();
-
-
-    start_loop(cpu, screen, &display, system, font, || {
+    start_loop(cpu, screen, &display, || {
         for event in display.poll_events() {
             match event {
                 glutin::Event::Closed => {
@@ -78,15 +64,11 @@ enum Action {
 fn start_loop<F>(mut cpu: Cpu<NesMemoryImpl>,
                  screen: Rc<RefCell<NesScreen>>,
                  display: &glium::Display,
-                 text_system: TextSystem,
-                 font: FontTexture,
                  mut callback: F)
     where F: FnMut() -> Action
 {
     let mut accumulator = Duration::new(0, 0);
     let mut previous_clock = Instant::now();
-    let mut fps = 0.0_f32;
-    let fps_smoothing = 0.995_f32;
 
     loop {
         match callback() {
@@ -99,15 +81,11 @@ fn start_loop<F>(mut cpu: Cpu<NesMemoryImpl>,
         previous_clock = now;
 
         let fixed_time_stamp = Duration::new(0, 16666667);
-        let frame_start = time::precise_time_ns();
         while accumulator >= fixed_time_stamp {
             accumulator -= fixed_time_stamp;
             loop {
                 if cpu.step().unwrap() == Interrupt::Nmi {
-                    let frame_time = time::precise_time_ns() - frame_start;
-                    let new_fps = NS_PER_SEC / frame_time as f32;
-                    fps = (fps * fps_smoothing) + (new_fps * (1.0 - fps_smoothing));
-                    update_screen(&display, &screen, &text_system, &font, fps);
+                    update_screen(&display, &screen);
                     break;
                 }
             }
@@ -116,39 +94,18 @@ fn start_loop<F>(mut cpu: Cpu<NesMemoryImpl>,
     }
 }
 
-fn update_screen(display: &Display,
-                 screen: &Rc<RefCell<NesScreen>>,
-                 text_system: &TextSystem,
-                 font: &FontTexture,
-                 fps: f32) {
-    let mut target = display.draw();
+fn update_screen(display: &Display, screen: &Rc<RefCell<NesScreen>>) {
+    let target = display.draw();
 
     // Write screen buffer
-    {
-        let borrowed_scr: NesScreen = screen.borrow().to_owned();
-        let mut buf = vec![0_u8; 256 * 240 * 3];
-        buf.clone_from_slice(&borrowed_scr.screen_buffer[..]);
-        let screen = RawImage2d::from_raw_rgb_reversed(buf, SCREEN_DIMENSIONS);
-        glium::Texture2d::new(display, screen)
-            .unwrap()
-            .as_surface()
-            .fill(&target, MagnifySamplerFilter::Nearest)
-    };
+    let borrowed_scr: NesScreen = screen.borrow().to_owned();
+    let mut buf = vec![0_u8; 256 * 240 * 3];
+    buf.clone_from_slice(&borrowed_scr.screen_buffer[..]);
+    let screen = RawImage2d::from_raw_rgb_reversed(buf, SCREEN_DIMENSIONS);
+    glium::Texture2d::new(display, screen)
+        .unwrap()
+        .as_surface()
+        .fill(&target, MagnifySamplerFilter::Nearest);
 
-    // Write diagnostic text
-    {
-        let text = glium_text::TextDisplay::new(text_system, font, &format!("FPS:{:.0}", fps));
-        let (w, h) = display.get_framebuffer_dimensions();
-        // Finally, drawing the text is done like this:
-        let matrix = [[0.05, 0.0, 0.0, 0.0],
-                      [0.0, 0.05 * (w as f32) / (h as f32), 0.0, 0.0],
-                      [0.0, 0.0, 1.0, 0.0],
-                      [-1.0, 0.0, 0.0, 1.0]];
-        glium_text::draw(&text,
-                         &text_system,
-                         &mut target,
-                         matrix,
-                         (1.0, 1.0, 0.0, 1.0));
-    }
     target.finish().unwrap();
 }
